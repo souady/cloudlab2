328982753d27b2e2d70f56ac485a838befbfcace
Alfonso Caschili <alfo@gator.it>
2019-07-18 07:48:56 +0200

    added newline

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Cloud Lab";
+        return "Hello Cloud Lab\n";
     }
 
     public static void main(String[] args) {
