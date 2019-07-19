8c629d7b1f54d9794e1bd0de489dcbff4760ed4f
Alfonso Caschili <alfo@gator.it>
2019-07-19 09:16:01 +0200

    friday

--- src/main/java/hello/Repository/EventRepository.java
+++ src/main/java/hello/Repository/EventRepository.java
@@ -1,4 +1,14 @@
 package hello.Repository;
 
-public class EventRepository {
+import hello.model.Event;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "event", path = "event")
+public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+    List<Event> findByName(@Param("name") String name);
 }
--- src/main/java/hello/model/Event.java
+++ src/main/java/hello/model/Event.java
@@ -1,4 +1,43 @@
 package hello.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "events")
 public class Event {
+
+    public Event() {}
+
+    public Event(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
 }
